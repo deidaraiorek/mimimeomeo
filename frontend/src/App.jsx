3d945a29c217aa470f335f10c09c3ba8f7579434
Huu Dang Pham <danghuupham@usf.edu>
2024-10-04 19:01:14 -0400

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,18 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import CoupleNote from "./pages/CoupleNote";
 import { useState } from "react";
 import "./App.css";
-import Header from './components/Header';
+import Header from "./components/Header";
 import LandingPage from "./pages/LandingPage";
-
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/couplenote" element={<CoupleNote />} />
         <Route path="/" element={<LandingPage />} />
       </Routes>
-  </Router>
+    </Router>
   );
 }
 
