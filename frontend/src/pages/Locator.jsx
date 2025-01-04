1fda41cd5a419e991856cc06db7d576dc5cf323e
deidaraiorek <100989552+deidaraiorek@users.noreply.github.com>
2025-01-04 16:51:19 +0700

    Update Locator.jsx

--- frontend/src/pages/Locator.jsx
+++ frontend/src/pages/Locator.jsx
@@ -8,11 +8,12 @@ import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
+// Fix for resolving paths correctly in production
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: markerIcon2x,
-  iconUrl: markerIcon,
-  shadowUrl: markerShadow,
+  iconRetinaUrl: new URL(markerIcon2x, import.meta.url).toString(),
+  iconUrl: new URL(markerIcon, import.meta.url).toString(),
+  shadowUrl: new URL(markerShadow, import.meta.url).toString(),
 });
 
 const Locator = () => {
