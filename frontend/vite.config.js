75d6b64a629e533e72561365f1b9daf53559aee1
deidaraiorek <100989552+deidaraiorek@users.noreply.github.com>
2025-01-04 16:47:03 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      leaflet: 'leaflet/dist/leaflet.js',
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['leaflet/dist/leaflet.css'],
+    },
+  },
+});
